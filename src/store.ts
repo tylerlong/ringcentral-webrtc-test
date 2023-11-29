384d562aa717a4a7801ebe7217efdf7d48951dd1
Tyler Liu <tyler4long@gmail.com>
2023-11-29 10:41:53 -0800

    Print messages to console

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
-import { manage } from 'manate';
+import { manage, autoRun } from 'manate';
 
 export class Store {
   public messages = ['Hello, world!'];
 }
 
 const store = manage(new Store());
+
+const { start } = autoRun(store, () => {
+  console.log(store.messages[store.messages.length - 1]);
+});
+start();
 
 export default store;
